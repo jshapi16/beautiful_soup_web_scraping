8e26affcf81b4224b156ced347bd48850500fd09
jshapi16 <jord.shapiro@gmail.com>
2021-07-08 09:41:54 -0700

    updating scrape_mars

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -14,15 +14,14 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
-
+    mars_data = mongo.db.mars_data.find_one()
+    return render_template("index.html", mars_data=mars_data)
 
 @app.route("/scrape")
 def scraper():
-    mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
-    mars.update({}, mars_data, upsert=True)
+    mars_data = mongo.db.mars_data
+    mars_db = scrape_mars.scrape_all()
+    mars_db.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,173 +1,79 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[27]:
-
-
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import requests
-
-
-# In[28]:
-
-
-#set up executable path
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# ## Scrape news from redplanet website
-
-# In[29]:
-
-
-#setup beautiful soup for redplanetscience.com
-
-#URL to be scraped
-url = "https://www.redplanetscience.com"
-
-#visit url
-browser.visit(url)
-
-#Grab the html 
-news_html = browser.html
-
-#create beautiful soup object with parser - parse in the html elements
-soup = BeautifulSoup(news_html, "html.parser")
-
-
-# In[30]:
-
-
-#find most recent news title
-news_title = soup.find('div', class_="content_title").text
-news_title
-
-
-# In[34]:
-
-
-#find paragraph text
-news_p = soup.find('div', class_="article_teaser_body").text
-news_p
-
-
-# In[35]:
-
-
-#Make a dictionary with the values
-redplanet_dict = {"news_title": "NASA's Perseverance Rover 100 Days Out", "news_p": "Mark your calendars: The agency's latest rover has only about 8,640,000 seconds to go before it touches down on the Red Planet, becoming history's next Mars car."}
-redplanet_dict
-
-
-# ## Scrape photo from mars space image website
-
-# In[78]:
-
-
-#set up url for. the browser to visit using splinter
-url="https://spaceimages-mars.com/"
-html = browser.visit(url)
-
-
-# In[79]:
-
-
-#locate the button and click it
-image_element = browser.find_by_tag("button")[1]
-image_element.click()
-
-
-# In[80]:
-
-
-#Parse with beautiful soup
-html = browser.html
-img_soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[83]:
-
-
-#Select image src and add it to the url
-image_anchor = img_soup.find("img", class_="fancybox-image").get('src')
-featured_image_url = url + image_anchor
-print(featured_image_url)
-print(image_anchor)
-
-
-# In[40]:
-
-
-#Make a dictionary of space image
-space_image_dict = {"featured_image": "https://spaceimages-mars.com/image/featured/mars3.jpg"}
-space_image_dict
-
-
-# ## Scrape hemisphere data from mars hemispheres
-
-# In[22]:
-
-
-#Visit mars hemispheres website
-url = "https://www.marshemispheres.com"
-browser.visit(url)
-
-
-# In[23]:
-
-
-#List which will hold the dictionaries
-hemisphere_image_links = []
-
-
-#get links for all hemispheres
-links = browser.find_by_css('a.product-item img')
-
-#click all links and get the href
-for i in range(len(links)):
-    hemisphere_dict = {}
-    browser.find_by_css('a.product-item img')[i].click()
-    thumb_img = browser.links.find_by_text('Sample').first
-    print(thumb_img)
-    
-    #hemisphere href
-    hemisphere_dict['img_url'] = thumb_img['href']
-    
-    #hemisphere title
-    hemisphere_dict['title'] = browser.find_by_css('h2.title').text
-    
-    hemisphere_image_links.append(hemisphere_dict)
-    
-    browser.back()
-
-
-# In[68]:
-
-
-#checking the list and dict
-hemisphere_image_links
-hemisphere_dict
-
-
-# # Combine all dicts
-
-# In[91]:
-
+import pandas as pd
 
 def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
-    data = {
+    
+    #Redplanetscience
+    #URL to be scraped
+    url = "https://www.redplanetscience.com"
+    #visit url
+    browser.visit(url)
+    #Grab the html 
+    news_html = browser.html
+    #create beautiful soup object with parser - parse in the html elements
+    soup = BeautifulSoup(news_html, "html.parser")
+    #find most recent news title
+    news_title = soup.find('div', class_="content_title").text
+    #find paragraph text
+    news_p = soup.find('div', class_="article_teaser_body").text
+    
+    
+    #Space images, featured image 
+    url="https://spaceimages-mars.com/"
+    html = browser.visit(url)
+    #locate the button and click it
+    image_element = browser.find_by_tag("button")[1]
+    image_element.click()
+    #Parse with beautiful soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+    #Select image src and add it to the url
+    image_anchor = img_soup.find("img", class_="fancybox-image").get('src')
+    featured_image_url = url + image_anchor
+    return featured_image_url
+    
+    def mars_facts():
+        mars_table = pd.read_html('https://galaxyfacts-mars.com')[0]
+        mars_table.columns = ['description', 'mars', 'earth']
+        mars_table.set_index('description', inplace=True)
+        mars_df = mars_table.to_html()
+        return mars_df
+
+    #mars hemispheres information
+    def mars_hemispheres():
+        #Visit mars hemispheres website
+        url = 'https://marshemispheres.com/'
+        browser.visit(url)
+        hemisphere_image_links = []
+
+        #get links for all hemispheres
+        links = browser.find_by_css('a.product-item img')
+        hemisphere_dict = {}
+        #click all links and get the href
+        for i in range(len(links)):
+            browser.find_by_css('a.product-item img')[i].click()
+            thumb_img = browser.links.find_by_text('Sample').first
+            print(thumb_img)
+            #hemisphere href
+            hemisphere_dict['img_url'] = thumb_img['href']
+            #hemisphere title
+            hemisphere_dict['title'] = browser.find_by_css('h2.title').text
+            hemisphere_image_links.append(hemisphere_dict)
+            browser.back()
+        return hemisphere_image_links
+
+    mars_data = {
         "news_title": news_title,
         "news_p": news_p,
         "featured_image": featured_image_url(browser),
-        "facts": mars_facts(),
-        "hemispheres": hemispheres(browser),
+        "mars_facts": mars_facts(),
+        "hemispheres": mars_hemispheres(browser),
     }
     browser.quit()
-    return(data)
-
+    return(mars_data)
